6e294fd3338cbcfc2a3d799cc2f500feed6b66d7
DaeHyung Lee <daehyung@gmail.com>
2015-10-07 11:26:59 +0900

    update for firewall

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,13 +1,12 @@
 class profiles::base {
-  $_motd_content =  "__________________________________________________________
-/ Q:      What's tiny and yellow and very, very, dangerous? \
-\ A:      A canary with the super-user password.            /
- -----------------------------------------------------------
-        \   ^__^
-         \  (oo)\_______
-            (__)\       )\/\
-                ||----w |
-                ||     || "  
+  $_motd_content =  "\n* * * * * * * * * W A R N I N G * * * * * * * * * * *
+This system is restricted to authorized users for
+authorized use only. Unauthorized access is strictly
+prohibited and may be punishable under the Computer
+Fraud and Abuse Act of 1984 or other applicable laws.
+All persons are hereby notified that the use of this
+system constitutes consent to monitoring and auditing.
+* * * * * * * * * * * * * * * * * * * * * * * * * * *\n"
 
   file { '/etc/motd':
     ensure  => file,
