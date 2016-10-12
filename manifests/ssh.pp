cd7a027f7e140d7997df17722d71429f271b4bed
Daehyung Lee <daehyung@gmail.com>
2016-10-12 17:05:13 +0900

    Update SELinux for SSH

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -13,14 +13,17 @@ class profiles::ssh(
   $options = hiera_hash('ssh::server_options')
   $ports = $options['Port']
 
+  # Set SELinux for SSH
+  # This will add ports into ssh_port_t context
+  include  '::selinux'
   $ports.each |String $port| {
     ::selinux::port { "selinux_ssh_${port}":
-      context  => 'ssh_port',
+      context  => 'ssh_port_t',
       port     => $port,
       protocol => 'tcp',
     }
   }
-  contain 'selinux::port'
+
   include ::ssh
 
 }
