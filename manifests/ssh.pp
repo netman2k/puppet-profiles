61313720bf468d948b54a000725f3eff3a522ddd
Daehyung Lee <daehyung@gmail.com>
2016-10-12 16:54:05 +0900

    updated for selinux

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -10,6 +10,17 @@
 class profiles::ssh(
 
 ){
+  $options = hiera_hash('ssh::server_options')
+  $ports = $options['Port']
 
+  $ports.each |String $port| {
+    selinux::port { "selinux_ssh_${port}":
+      context  => 'ssh_port',
+      port     => $port,
+      protocol => 'tcp',
+    }
+  }
+  contain 'selinux::port'
   include ::ssh
+
 }
