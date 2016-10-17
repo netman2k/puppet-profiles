f356635d3f25d715ef539a5a37b4d55d046cbd88
Daehyung Lee <daehyung@gmail.com>
2016-10-17 11:08:47 +0900

    updated /etc/services for nrpe

---
+++ manifests/monitoring/nrpe.pp
@@ -0,0 +1,21 @@
+# Class: Profiles::monitoring::nrpe
+#   This class set basic NRPE settings
+## Parameters
+#
+## Variables
+#
+## Authors
+#   Daehyung.lee <daehyung@gmail.com>
+class profiles::monitoring::nrpe {
+
+  $nrpe_ports = hiera_array('profiles::monitoring::nrpe::ports', [])
+
+  # Adds service ports into /etc/services
+  $nrpe_ports.each |Integer $port|{
+    ::etc_services { "nrpe-${port}/tcp":
+      port    => String($port, "%d"),
+      aliases => [ 'nagios-nrpe', 'nrpe' ],
+      comment => 'The Nagios NRPE for CDNetworks'
+    }
+  }
+}
