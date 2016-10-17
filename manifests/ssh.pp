b5c32712f5f0fc57fab69326767b18f20459a409
Daehyung Lee <daehyung@gmail.com>
2016-10-17 14:18:52 +0900

    updated

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -14,12 +14,12 @@ class profiles::ssh {
 
   include  '::selinux'
 
-  $ports.each |Integer $port| {
+  $ports.each |String $port| {
     # Set SELinux for SSH
     # This will add ports into ssh_port_t context
     ::selinux::port { "selinux_ssh_${port}":
       context  => 'ssh_port_t',
-      port     => $port,
+      port     => Integer($port),
       protocol => 'tcp',
       tag      => [ 'selinux_port' ]
     }
