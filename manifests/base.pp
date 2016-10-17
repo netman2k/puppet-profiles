66f5a2a76c5b9b1464726a8f61f52fee1c65336e
Daehyung Lee <daehyung@gmail.com>
2016-10-17 13:32:18 +0900

    add common services for /etc/services

--- manifests/base.pp
+++ manifests/base.pp
@@ -10,6 +10,7 @@ class profiles::base {
 
   $motd_content =  hiera('profiles::base::motd')
   $login_defs_options = hiera_hash('profiles::base::login_defs::options')
+  $services = hiera_array('profiles::base::services')
 
   file { '/etc/motd':
     ensure  => file,
@@ -20,5 +21,22 @@ class profiles::base {
     options => $login_defs_options,
   }
 
-  
+  # Adds service ports into /etc/services
+  $services.each | Hash $svc | {
+    $_name = $svc['name']
+    $_protocol = $svc['protocol']
+    $_port = String($svc['port'], "%d")
+    $_aliases = empty($svc['aliases']) ? {
+      true  => [],
+      false => $svc['aliases']
+    }
+    $_aliases = $svc['aliases']
+    $_comment = $svc['comment']
+    ::etc_services { "${_name}/${_protocol}":
+      port    => $_port,
+      aliases => $_aliases,
+      comment => $_comment,
+    }
+  }
+
 }
