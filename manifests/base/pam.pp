428e6468913cc7c29a46dd8cc0f2fb44f97e0752
Daehyung Lee <daehyung@gmail.com>
2016-10-20 13:40:40 +0900

    adds comment about the module

--- manifests/base/pam.pp
+++ manifests/base/pam.pp
@@ -1,5 +1,10 @@
 # Class: profiles::base::pam
 #  This class manages content of the pam related stuff
+#  Internally, this class uses the ghoneycutt/pam module.
+#  this module set many things via hiera mostly
+#  so you need to look at the manual how to set and
+#  what's going on when you run it.
+#  https://github.com/ghoneycutt/puppet-module-pam
 #
 ## Parameters
 #
@@ -9,8 +14,6 @@
 # 	Daehyung Lee <daehyung@gmail.com>
 class profiles::base::pam(){
 
-
     # The below will lookup the values via hiera
-    # https://github.com/ghoneycutt/puppet-module-pam
     class { '::pam': }
 }
